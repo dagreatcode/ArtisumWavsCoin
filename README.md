34bdf5b7c5e10facde77f5dd2e27826f47f0cf97
dagreatcode <dagreatcode@gmail.com>
2024-08-05 00:53:15 -0400

    added the Block

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ArtisumWavsCoin
+
+https://www.youtube.com/watch?v=mslUjcbzhwA&t=117s
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 
 class Transaction {
     constructor(
@@ -6,16 +6,28 @@ class Transaction {
         public payer: string,
         public payee: string
     ) {}
-
+    // Method to call
     toString() {
         return JSON.stringify(this)
     }
 }
-
+// Container with multi transactions. Leger. 1mb of info. Like a Linked List without being manipulated. timestamp. Nunce Number only used once.
 class Block {
-    constructor(
 
+    public numOnlyUsedOnce = Math.round(Math.random() * 999999999);
+
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public ts = Date.now()
     ) {}
+
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex')
+    }
 }
 
 class Chain {
