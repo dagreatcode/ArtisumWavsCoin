73c4eb4750ef6187cc10c4d3dea47f5ae7623287
dagreatcode <dagreatcode@gmail.com>
2024-08-04 20:33:30 -0400

    added the classes needed to et started.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Transaction {
+    constructor(amount, payer, payee) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+    }
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+class Block {
+    constructor() { }
+}
+class Chain {
+}
+class Wallet {
+    constructor() {
+    }
+}
---
+++ index.ts
@@ -0,0 +1,29 @@
+import * as crypto from 'crypto';
+
+class Transaction {
+    constructor(
+        public amount: number,
+        public payer: string,
+        public payee: string
+    ) {}
+
+    toString() {
+        return JSON.stringify(this)
+    }
+}
+
+class Block {
+    constructor(
+
+    ) {}
+}
+
+class Chain {
+
+}
+
+class Wallet {
+    constructor() {
+
+    }
+}
